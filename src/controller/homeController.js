6f182c8dba011a8b999abaffcf9614ae057581b7
leminhtri <leminhtri900112@gmail.com>
2022-11-11 19:38:43 +0700

    connect database and render table

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,16 @@
+import connection from "../configs/connectDB";
 let getHomePage = (req, res) => {
   //logic
-
-  return res.render("index.ejs");
+  let data = [];
+  connection.query("SELECT * FROM `users`", function (err, results, fields) {
+    console.log(">>>get DB");
+    console.log(results); // results contains rows returned by server
+    data = results.map((row) => {
+      return row;
+    });
+    //console.log(fields); // fields contains extra meta data about results, if available
+    return res.render("index.ejs", { dataUser: JSON.stringify(data) });
+  });
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRouter from "./router/web";
+import connection from "./configs/connectDB";
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
